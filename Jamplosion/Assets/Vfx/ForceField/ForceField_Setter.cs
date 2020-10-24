5c25fe03e5637c615026718392d39e443537f14a
teppich <johannesspicker@me.com>
2020-10-24 22:14:12 +0200

    Knob Riddle

--- Jamplosion/Assets/Vfx/ForceField/ForceField_Setter.cs
+++ Jamplosion/Assets/Vfx/ForceField/ForceField_Setter.cs
@@ -3,39 +3,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshRenderer))]
-public class ForceField_Setter : MonoBehaviour
+namespace Vfx.ForceField
 {
-    [ColorUsage(true, true)] [SerializeField]
-    private Color standardColor;
-
-    [ColorUsage(true, true)] [SerializeField]
-    private Color alertColor;
-
-    private string offset = "Vector1_AEA6E811";
-    private string emission = "Color_FE400732";
-    private string fresnelPower = "Vector1_E4D88293";
-    private string scrollPower = "Vector1_4D3EB88F";
-    private string pattern = "Texture2D_7E12B800";
-
-    private MeshRenderer _renderer;
-
-    private void Awake()
+    [RequireComponent(typeof(MeshRenderer))]
+    public class ForceField_Setter : MonoBehaviour
     {
-        _renderer = GetComponent<MeshRenderer>();
+#pragma warning disable CS0414
+#pragma warning disable CS0649
+        [ColorUsage(true, true)] [SerializeField]
+        private Color standardColor;
+
+        [ColorUsage(true, true)] [SerializeField]
+        private Color alertColor;
+
+        private string offset = "Vector1_AEA6E811";
+        private string emission = "Color_FE400732";
+        private string fresnelPower = "Vector1_E4D88293";
+        private string scrollPower = "Vector1_4D3EB88F";
+        private string pattern = "Texture2D_7E12B800";
+
+        private MeshRenderer _renderer;
+
+#pragma warning restore CS0141
+#pragma warning restore CS0649
+
+        private void Awake()
+        {
+            _renderer = GetComponent<MeshRenderer>();
+        }
+
+        private void Start() => SetStandardColor();
+
+        void Update()
+        {
+            _renderer.material.SetFloat(offset, 1f + 0.15f * Mathf.Sin(Time.time / 8f));
+        }
+
+        private void OnMouseEnter() => SetAlertColor();
+
+        private void OnMouseExit() => SetStandardColor();
+        private void SetStandardColor() => _renderer.material.SetColor(emission, standardColor);
+
+        private void SetAlertColor() => _renderer.material.SetColor(emission, alertColor);
     }
-
-    private void Start() => SetStandardColor();
-
-    void Update()
-    {
-        _renderer.material.SetFloat(offset, 1f + 0.15f * Mathf.Sin(Time.time / 8f));
-    }
-
-    private void OnMouseEnter() => SetAlertColor();
-
-    private void OnMouseExit() => SetStandardColor();
-    private void SetStandardColor() => _renderer.material.SetColor(emission, standardColor);
-
-    private void SetAlertColor() => _renderer.material.SetColor(emission, alertColor);
 }
---
+++ Jamplosion/Assets/Vfx/KnobRiddle/KnobRiddle_Setter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace Vfx.KnobRiddle
+{
+    [RequireComponent(typeof(MeshRenderer))]
+    public class KnobRiddle_Setter : MonoBehaviour
+    {
+#pragma warning disable CS0414
+#pragma warning disable CS0649
+
+        private string input1 = "Vector1_8FC680A2";
+        private string input2 = "Color_FE400732";
+        private string input3 = "Vector1_E4D88293";
+        private string speed = "Vector1_51541A47";
+
+        private MeshRenderer _renderer;
+
+#pragma warning restore CS0141
+#pragma warning restore CS0649
+
+        private void Awake()
+        {
+            _renderer = GetComponent<MeshRenderer>();
+        }
+
+        public void SetInput1(float value) => _renderer.material.SetFloat(input1, value);
+        public void SetInput2(float value) => _renderer.material.SetFloat(input2, value);
+        public void SetInput3(float value) => _renderer.material.SetFloat(input3, value);
+    }
+}
