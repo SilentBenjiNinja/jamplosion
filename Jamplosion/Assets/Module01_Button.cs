34529f6c7669228ebc95ba4538f9d983ac933780
lolesch <leonid.schreiber@yahoo.de>
2020-10-24 16:19:30 +0200

    Module 01 is ready to go

---
+++ Jamplosion/Assets/Module01_Button.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Renderer))]
+public class Module01_Button : MonoBehaviour
+{
+    private Renderer rend;
+    private Module01_Observer observer;
+
+    public bool isOn = false;
+
+    [SerializeField] private Color hoverColor = new Color(.5f, .5f, .25f);
+
+    [SerializeField] private Module01_Button[] neighbors;
+
+
+    void Start()
+    {
+        observer = GetComponentInParent<Module01_Observer>();
+
+        rend = GetComponent<Renderer>();
+        ColorCheck();
+    }
+
+    void OnMouseEnter()
+    {
+        Color sensitiveEmission = rend.material.color + hoverColor;
+        rend.material.SetColor("_EmissionColor", sensitiveEmission);
+    }
+
+    private void OnMouseDown()
+    {
+        isOn = !isOn;
+
+        ColorCheck();
+
+        foreach (var button in neighbors)
+        {
+            button.isOn = !button.isOn;
+            button.ColorCheck();
+        }
+
+        Color sensitiveEmission = rend.material.color + hoverColor;
+        rend.material.SetColor("_EmissionColor", sensitiveEmission);
+
+        observer.SomethingChanged();
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.SetColor("_EmissionColor", Color.gray);
+    }
+
+    void ColorCheck()
+    {
+        if (isOn) rend.material.color = Color.green;
+        else rend.material.color = Color.red;
+        //rend.material.SetColor("_EmissionColor", Color.gray);
+    }
+
+    void OnMouseOver()
+    {
+    }
+
+    private void OnMouseUp()
+    {
+    }
+}
---
+++ Jamplosion/Assets/Prefabs/Module01_PuzzlePresets.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Module01_PuzzlePresets : ScriptableObject
+{
+    //[SerializeField] public bool[][] puzzles;
+    public Puzzle[] puzzles;
+
+    public bool[] GetRandomPuzzlePreset()
+    {
+        int i = UnityEngine.Random.Range(0, puzzles.Length);
+
+        return puzzles[i].preset;
+    }
+
+    [Serializable]
+    public class Puzzle
+    {
+        public bool[] preset;
+    }
+
+    private void OnValidate()
+    {
+        foreach (var puzzle in puzzles)
+        {
+            if (puzzle.preset != null && puzzle.preset.Length != 16)
+            {
+                puzzle.preset = new bool[16];
+            }
+        }        
+    }
+}
