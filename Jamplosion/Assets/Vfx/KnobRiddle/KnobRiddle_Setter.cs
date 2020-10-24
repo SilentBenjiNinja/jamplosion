8bbc0f7667e0ea5bfe54170e026f0dbf4d482f35
teppich <johannesspicker@me.com>
2020-10-24 22:34:06 +0200

    Knob Riddle Accessor
    - can now set shader values via slider

--- Jamplosion/Assets/Vfx/KnobRiddle/KnobRiddle_Setter.cs
+++ Jamplosion/Assets/Vfx/KnobRiddle/KnobRiddle_Setter.cs
@@ -10,11 +10,16 @@ namespace Vfx.KnobRiddle
 #pragma warning disable CS0649
 
         private string input1 = "Vector1_8FC680A2";
-        private string input2 = "Color_FE400732";
-        private string input3 = "Vector1_E4D88293";
+        private string input2 = "Vector1_B91E0812";
+        private string input3 = "Vector1_88DC087B";
         private string speed = "Vector1_51541A47";
 
         private MeshRenderer _renderer;
+
+        [SerializeField] [Range(0f, 1f)] private float knob1 = 0.5f;
+        [SerializeField] [Range(0f, 1f)] private float knob2 = 0.5f;
+        [SerializeField] [Range(0f, 1f)] private float knob3 = 0.5f;
+        [SerializeField] [Range(1f, 10f)] private float speedValue = 1f;
 
 #pragma warning restore CS0141
 #pragma warning restore CS0649
@@ -27,5 +32,14 @@ namespace Vfx.KnobRiddle
         public void SetInput1(float value) => _renderer.material.SetFloat(input1, value);
         public void SetInput2(float value) => _renderer.material.SetFloat(input2, value);
         public void SetInput3(float value) => _renderer.material.SetFloat(input3, value);
+        public void SetSpeed(float value) => _renderer.material.SetFloat(speed, value);
+
+        private void Update()
+        {
+            SetInput1(knob1);
+            SetInput2(knob2);
+            SetInput3(knob3);
+            SetSpeed(speedValue);
+        }
     }
 }
