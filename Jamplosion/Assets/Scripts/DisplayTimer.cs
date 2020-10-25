ea24b63c3987557eba7219370613b5a448d95d6f
SG Benjamin Rosenberger <benjamin.rosenberger@softgames.de>
2020-10-25 15:01:35 +0100

    import sounds; add timer prefab

--- Jamplosion/Assets/Scripts/DisplayTimer.cs
+++ Jamplosion/Assets/Scripts/DisplayTimer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,11 +8,11 @@ public class DisplayTimer : MonoBehaviour
 {
     public GameManager gm;
 
-    Text txtTimer;
+    TextMeshProUGUI txtTimer;
 
     private void Awake()
     {
-        txtTimer = GetComponent<Text>();
+        txtTimer = GetComponent<TextMeshProUGUI>();
     }
 
     void Update()
