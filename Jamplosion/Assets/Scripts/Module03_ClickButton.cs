aa2520e7404ed0af2a234aa43d72678a0916b053
Clemens <clemensspitzer@gmx.at>
2020-10-25 14:50:56 +0100

    Added sound files

---
+++ Jamplosion/Assets/Scripts/Module03_ClickButton.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Module03_ClickButton : MonoBehaviour
+{  
+    public Material lmat;
+    public Material nMat;
+
+    private Renderer myR;
+
+    public int startCount = 3;
+
+    public int myNumber;
+
+    public delegate void ClickEV(int number);
+
+    public event ClickEV onClick;
+
+    void Awake()
+    {
+        myR = GetComponent<Renderer>();
+        myR.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        ClickedColor();
+    }
+
+    private void OnMouseUp()
+    {
+        UnClickedColor();
+    }
+
+    public void ClickedColor()
+    {
+        myR.sharedMaterial = nMat;
+    }
+
+    public void UnClickedColor()
+    {
+        myR.sharedMaterial = lmat;
+    }
+}
